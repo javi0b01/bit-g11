2503dba06a44d97912157ac39cdff4cf8fccdd2c
javi <55107141+javi0b01@users.noreply.github.com>
2025-12-10 21:04:57 -0500

    clase 6

---
+++ clase6/script.js
@@ -0,0 +1,14 @@
+/* Referencias a elementos del DOM */
+/* constantes */
+const $guessInput = document.getElementById('guessInput');
+const $guessButton = document.getElementById('guessButton');
+const $message = document.getElementById('message');
+const $restartButton = document.getElementById('restartButton');
+
+/* Escuchar eventos */
+document.addEventListener('DOMContentLoaded', initializeGame);
+
+// inicializar el juego
+function initializeGame() {
+  console.log('works');
+}
