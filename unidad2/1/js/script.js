e46a7df507b5381ceafe9b81f5b3b0f820105bd9
javi <55107141+javi0b01@users.noreply.github.com>
2026-01-28 21:03:27 -0500

    Updated

--- unidad2/1/js/script.js
+++ unidad2/1/js/script.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const welcomeMessage = document.getElementById('welcome-message');
   const categoriesDisplay = document.getElementById('unique-categories');
 
+  // Temas: Arreglos y local storage
   let tasks = [];
 
   /* Temas: Fechas e Intervalos */
@@ -19,25 +20,91 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   setInterval(updateClock, 1000);
 
+  // Tema: JSON.stringify convierte nuestro arreglo en string 'JSON'
   function saveTasks() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 
+  // Función para mostrar mis tareas en la UI
+  function renderTasks() {
+    // Limpiar el contenido previo
+    taskList.innerHTML = '';
+    console.log(tasks);
+    // Tema: ciclos ('foreach')
+    // Crear un elemento de lista por cada elemento en el arreglo
+    tasks.forEach((task, index) => {
+      // Crear un elemento de lista
+      const li = document.createElement('li');
+      // Agregar una clase de css
+      li.className = 'task-item';
+
+      // Si la tarea está marcada como completada
+      if (task.completed) {
+        li.classList.add('completed');
+      }
+
+      const taskText = document.createElement('span');
+      taskText.textContent = `${task.text} (Agregada: ${task.dateAdded})`;
+
+      const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = 'Eliminar';
+
+      li.addEventListener('click', () => {
+        toggleTaskCompletion(index);
+      });
+
+      deleteBtn.addEventListener('click', (event) => {
+        event.stopPropagation();
+        deleteTask(index);
+      });
+      // Agregar contenido al elemento de lista
+      li.appendChild(taskText);
+      li.appendChild(deleteBtn);
+      // Agregar contenido a la lista
+      taskList.appendChild(li);
+    });
+  }
+
+  function toggleTaskCompletion(index) {
+    tasks[index].completed = !tasks[index].completed;
+    // Guardar
+    saveTasks();
+    // Mostrar
+    renderTasks();
+  }
+
+  function deleteTask(index) {
+    tasks.splice(index, 1);
+    // Guardar
+    saveTasks();
+    // Mostrar
+    renderTasks();
+  }
+
+  // Temas: funciones, condicionales, objetos, arreglos, métodos integrados
+  // Esta función agrega nueva tarea al arreglo de tareas
   function addTask() {
+    // Tema: métodos integrados
     const text = taskInput.value.trim();
+    // Condicionales
     if (text === '') {
       alert('Por favor ingrese una tarea');
       return;
     }
-
+    // Tema: Objetos, Fechas
     const newTask = {
       text: text,
+      completed: false,
+      dateAdded: new Date().toLocaleDateString(),
     };
-
+    // Agregar elementos al arreglo
     tasks.push(newTask);
-
+    // Limpiar el input
     taskInput.value = '';
+    // Guardar
     saveTasks();
+    // Mostrar
+    renderTasks();
   }
 
   // Escuchar el evento "click" del botón
