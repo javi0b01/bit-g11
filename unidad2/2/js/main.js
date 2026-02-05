ce4f9787dd1fe5f8d142cd0afd2400f7865c54c8
javi <55107141+javi0b01@users.noreply.github.com>
2026-02-04 19:57:57 -0500

    Updated

--- unidad2/2/js/main.js
+++ unidad2/2/js/main.js
@@ -10,6 +10,7 @@ const names = [
   'Alejandro',
   'Camilo',
 ];
+console.log('names:', names);
 
 const originalNames = document.getElementById('original-names');
 
@@ -18,3 +19,40 @@ names.forEach((name) => {
   li.textContent = name;
   originalNames.appendChild(li);
 });
+
+/* Bucles - Ejemplos */
+// for
+const forLoopNames = [];
+for (let i = 0; i < names.length; i++) {
+  forLoopNames.push(names[i].toUpperCase());
+}
+console.log('forLoopNames:', forLoopNames);
+// while
+const whileLoopNames = [];
+let j = 0;
+while (j < names.length) {
+  whileLoopNames.push(names[j].toLowerCase());
+  j++;
+}
+console.log('whileLoopNames:', whileLoopNames);
+// for...of
+const forOfLoopNames = [];
+for (name of names) {
+  forOfLoopNames.push(name + ' 🙂');
+}
+console.log('forOfLoopNames:', forOfLoopNames);
+// for...in
+const forInLoopNames = [];
+for (const index in names) {
+  forInLoopNames.push(`😉 ${names[index]}`);
+}
+console.log('forInLoopNames:', forInLoopNames);
+
+const literalObject = {
+  username: 'demo',
+  password: 'ABC123',
+};
+for (const prop in literalObject) {
+  console.log('prop:', prop);
+  console.log('value:', literalObject[prop]);
+}
